4938d73e269a219e00ca3c0af9462f499f4f66f3
Wakil Israk Abir <wakilisrakabir@gmail.com>
2026-01-26 02:26:00 +0600

    task

--- 14.5-Array.js
+++ 14.5-Array.js
@@ -21,7 +21,7 @@ const n = [88, 70, 37, 40, 59, 75];
 
 // get element value by index
 const fourth = n[3];
-console.log(fourth);
+// console.log(fourth);
 
 //set or update element value by index
 n[1] = 34;
@@ -49,13 +49,66 @@ const out3 = friend.pop();
 
 //shift & unshift
 const student = ["Miraj", "Sakib", "Asik", "Yeamin"];
-console.log(student);
+// console.log(student);
 student.shift()
-console.log(student);
+// console.log(student);
 
 student.unshift("Nesar")
-console.log(student);
+// console.log(student);
 
 
 // All about basic array methods, isArray
 // --------------------------------------
+
+// includes
+const boys = ["Miraj", "Yeamin", "Takmin", "Nesar", "Asik"]
+// console.log(boys.includes("Miraj"));
+
+// if(boys.includes("Miraj")) {
+//     console.log("Lets go to Bandarban");
+// }else  {
+//     console.log("Then we are not going anywhere");
+// }
+
+// index
+// console.log(boys.indexOf("Asik"));
+// console.log(boys.indexOf("Tomato"))
+
+// check is array
+const food = "Ro sho gol la";
+// console.log(Array.isArray(food));
+// console.log(Array.isArray(boys));
+
+
+
+// Task
+// -----
+
+// 01.
+// const fruit = ["mango", "banana", "apple", "orange", "lichi"];
+// console.log(fruit.indexOf("orange"));
+// fruit[2] = "jambura";
+// console.log(fruit);
+
+// 02.
+// const spot = ["bandarban", "sundarban", "sylhet"];
+// spot.push("netrokona");
+// console.log(spot);
+// spot.push("cox-bazar");
+// spot.push("rajshahi");
+// console.log(spot);
+// spot.pop();
+// console.log(spot);
+
+// 03.
+// const book = ["bangla", "english", "math", "chemistry"];
+// console.log(book.includes("javascript"));
+
+// 04.
+// const check =[]
+// console.log(Array.isArray(check));
+
+// 05.Concatination of two array
+const farray = [2, 5, 8, 6];
+const sarray = [4, 5, 8, 9];
+console.log(farray.concat(sarray));
