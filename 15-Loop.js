6ded03765c0c5936ecd69a06b36677a54a7468e4
Wakil Israk Abir <wakilisrakabir@gmail.com>
2026-01-27 22:35:24 +0600

    Loop

---
+++ 15-Loop.js
@@ -0,0 +1,12 @@
+// 15-Loop.js
+const number = [12, 45, 87, 69, 35];
+console.log(number);
+for(const num of number){
+    console.log(num);
+}
+
+const fruits = ["apple", "banana", "orange"];
+for(const fruit of fruits) {
+    console.log(fruit);
+    console.log("I want to eat")
+}
---
+++ 15-while.js
@@ -0,0 +1,15 @@
+// 15-while.js
+
+const number = 7;
+if(number < 1) {
+    console.log("Yes we got smaller number");
+}
+console.log("something new is cooking");
+
+
+// while loop --------
+let num = 0;
+while(num <5) {
+    console.log("looping", num);
+    num = num + 1;
+}
